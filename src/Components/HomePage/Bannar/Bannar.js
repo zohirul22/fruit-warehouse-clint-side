f0750dcf5f8f41a30a73629bb6af1ff99659dceb
zohirulislam1 <codershahin84@gmail.com>
2022-11-25 00:52:27 +0600

    React and Router setup

---
+++ src/Components/HomePage/Bannar/Bannar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Bannar.css'
+
+const Bannar = () => {
+    return (
+        <div>
+           <h1>THis is bannar</h1> 
+        </div>
+    );
+};
+
+export default Bannar;
---
+++ src/Components/HomePage/Inventori/Inventori.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Inventori.css'
+
+const Inventori = () => {
+    return (
+        <div>
+            <h1>This is inventori</h1>
+        </div>
+    );
+};
+
+export default Inventori;
