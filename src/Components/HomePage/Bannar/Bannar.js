4b001d749793729770293fe3d2285111ff76dd6a
zohirulislam1 <codershahin84@gmail.com>
2022-11-26 00:01:27 +0600

    bannar section

--- src/Components/HomePage/Bannar/Bannar.js
+++ src/Components/HomePage/Bannar/Bannar.js
@@ -1,11 +1,54 @@
 import React from 'react';
-import './Bannar.css'
+import { Carousel } from 'react-bootstrap';
+import bannar_1 from '../../../../src/img/bannar/bannar_1.png';
+import bannar_2 from '../../../../src/img/bannar/bannar_2.png';
+import bannar_3 from '../../../../src/img/bannar/bannar_3.png';
+import './Bannar.css';
 
 const Bannar = () => {
     return (
-        <div>
-           <h1>THis is bannar</h1> 
-        </div>
+        <Carousel>
+            <Carousel.Item>
+                <img
+                    style={{ 'height': '400px' }}
+                    className="d-block w-100 "
+                    src={bannar_1}
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    style={{ 'height': '400px' }}
+                    className="d-block w-100"
+                    src={bannar_2}
+                    alt="Second slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    style={{ 'height': '400px' }}
+                    className="d-block w-100"
+                    src={bannar_3}
+                    alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>
+                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                    </p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
     );
 };
 
--- src/Components/HomePage/Home/Home.js
+++ src/Components/HomePage/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Bannar from '../Bannar/Bannar';
 import Header from '../Header/Header';
 
 const Home = () => {
     return (
         <div>
            <Header></Header>
+           <Bannar></Bannar>
          
         </div>
     );
